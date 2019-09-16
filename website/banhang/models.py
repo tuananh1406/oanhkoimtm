99eba7bbac7e7084c46c90409eb48005647b276c
Hữu Tuấn Anh <huutuananh1406@gmail.com>
2019-09-16 12:27:35 +0700

    Cập nhật thay đổi trong laptop

--- website/banhang/models.py
+++ website/banhang/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+class Sanpham(models.Model):
+    tensanpham = models.CharField("Tên sản phẩm", max_length=200)
+    hinhanh = models.ImageField("Hình ảnh", upload_to="sanpham/")
+    giatien = models.CharField("Giá tiền", max_length=50)
+    noidung_gioithieu = models.TextField("Nội dung giới thiệu")
+    ngaycapnhat = models.DateTimeField("Ngày cập nhật", auto_now=True)
+    nguoicapnhat = models.CharField("Người cập nhật", max_length=200)
+
+    def __str__(self):
+        return self.tensanpham
+
+class Baiviet(models.Model):
+    tieude = models.CharField("Tiêu đề", max_length=200)
+    noidung = models.TextField("Nội dung")
+    nguoiviet = models.CharField("Người viết bài", max_length=50)
+    danhmuc = models.CharField("Danh mục", max_length=200)
+    ngayvietbai = models.DateTimeField("Ngày viết bài", auto_now_add=True)
+    ngaycapnhat = models.DateTimeField("Ngày cập nhật", auto_now=True)
+
+    def __str__(self):
+        return self.tieude
--- website/website/settings.py
+++ website/website/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'banhang',
+    'banhang.apps.BanhangConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
