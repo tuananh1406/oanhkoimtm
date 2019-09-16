136a5107e2bcf30ff9f613bdb08179439ea07078
Hữu Tuấn Anh <huutuananh1406@gmail.com>
2019-09-16 14:38:36 +0700

    cập nhật model danh mục

--- website/banhang/models.py
+++ website/banhang/models.py
@@ -17,9 +17,21 @@ class Baiviet(models.Model):
     tieude = models.CharField("Tiêu đề", max_length=200)
     noidung = models.TextField("Nội dung")
     nguoiviet = models.CharField("Người viết bài", max_length=50)
-    danhmuc = models.CharField("Danh mục", max_length=200)
+    danhmuc = models.CharField("Danh mục", max_length=200,
+            default="None")
     ngayvietbai = models.DateTimeField("Ngày viết bài", auto_now_add=True)
     ngaycapnhat = models.DateTimeField("Ngày cập nhật", auto_now=True)
 
     def __str__(self):
         return self.tieude
+
+class Danhmuc(models.Model):
+    tendanhmuc = models.CharField("Tên danh mục", max_length=100)
+    gioithieu = models.CharField("Giới thiệu", max_length=200)
+    slug = models.CharField("Đường dẫn slug", max_length=200,
+            default=1)
+    class Meta:
+        verbose_name_plural = "Danh mục"
+
+    def __str__(self):
+        return self.tendanhmuc
